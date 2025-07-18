44c4d95a7f395e289760593b19118158bfc1b72f
ansilrahman777 <rahmanansil777@gmail.com>
2025-07-18 21:53:56 +0400

    changes in home page sec-09,03

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,7 +55,6 @@ const Home = ({ openBookingModal }) => {
         <HeroSection08 />
 
         <HeroSection09 />
-      </section>
       <div style={{ overflow: "hidden" }}>
         <svg
           preserveAspectRatio="none"
@@ -79,6 +78,7 @@ const Home = ({ openBookingModal }) => {
           <path d="M0 0v5.63C149.93 59 314.09 71.32 475.83 42.57c43-7.64 84.23-20.12 127.61-26.46 59-8.63 112.48 12.24 165.56 35.4C827.93 77.22 886 95.24 951.2 90c86.53-7 172.46-45.71 248.8-84.81V0z" />
         </svg>
       </div>
+      </section>
     </main>
   );
 };
